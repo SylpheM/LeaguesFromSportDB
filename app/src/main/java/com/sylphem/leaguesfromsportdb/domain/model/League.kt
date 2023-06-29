9fa257d318a2ca3e43e63270eec7cdcdbb881ad1
Stéphanie Luu <luu.stephanie@gmail.com>
2023-06-29 16:35:32 +0200

    Add domain models

---
+++ app/src/main/java/com/sylphem/leaguesfromsportdb/domain/model/League.kt
@@ -0,0 +1,6 @@
+package com.sylphem.leaguesfromsportdb.domain.model
+
+data class League(
+    val id: Long,
+    val name: String
+)
---
+++ app/src/main/java/com/sylphem/leaguesfromsportdb/domain/model/Team.kt
@@ -0,0 +1,7 @@
+package com.sylphem.leaguesfromsportdb.domain.model
+
+data class Team(
+    val id: Long,
+    val name: String,
+    val logo: String?
+)
